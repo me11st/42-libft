55fee72385ddc09a098e455560f9853ea8cba7e9
Mell Stefanova <mestefan@c4b7c6.42berlin.de>
2023-11-24 11:50:59 +0100

    add_ons

---
+++ ft_putnbr_fd_putchar.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mestefan <mestefan@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 22:46:46 by mestefan          #+#    #+#             */
+/*   Updated: 2023/11/21 22:46:49 by mestefan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+void 	putchar
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	x;
+
+	if (n < 0)
+	{
+		x = -n;
+		write(fd, "-", 1);
+	}
+	else
+		x = n;
+	if (x >= 10)
+		ft_putnbr_fd(x / 10, fd);
+	putchar(x % 10 + '0', fd);
+}
